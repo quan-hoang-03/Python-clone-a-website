7fbf2776acc06f1d9584a631f150731ce056604a
Quân Hoàng <quanhoang789a@gmail.com>
2024-10-26 19:18:20 +0700

    Fix : logout url

--- EPU_QLBH/app/urls.py
+++ EPU_QLBH/app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('account/login/',auth_view.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name="login"),
     path('passwordchange/',auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html',form_class=PasswordChangeForm,success_url='/passwordchangedone'),name="passwordchange"),
     path('passwordchangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'),name="passwordchangedone"),
-    # path('logout/',auth_view.LogoutView.as_view(next_page='login'),name="logout"),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name="logout"),
 
     path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=PasswordResetForm),name="password_reset"),
     path('password-reset/done',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name="password_reset_done"),
